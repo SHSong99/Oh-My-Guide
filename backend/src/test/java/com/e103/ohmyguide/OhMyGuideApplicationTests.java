3dd8d76be364df132ebbb168b4261a85b765bc6a
전준완 <joownan1024@naver.com>
2026-03-13 17:49:11 +0900

    chore: init backend project

---
+++ backend/src/test/java/com/e103/ohmyguide/OhMyGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e103.ohmyguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMyGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
