9aef6542e4c2c305960c210e6b085ee179b103cd
송성현 <99pyues@naver.com>
2026-03-19 12:21:40 +0900

    feature : 안드로이드용 구글SDK 방식 OAuth2 JWT 소셜로그인 API

--- backend/src/main/java/com/e103/ohmyguide/domain/auth/oauth2/CustomOAuth2UserService.java
+++ backend/src/main/java/com/e103/ohmyguide/domain/auth/oauth2/CustomOAuth2UserService.java
@@ -1,12 +1,10 @@
 package com.e103.ohmyguide.domain.auth.oauth2;
 
-
 import com.e103.ohmyguide.domain.auth.security.UserPrincipal;
+import com.e103.ohmyguide.domain.auth.service.OAuth2UserProcessingService;
 import com.e103.ohmyguide.domain.auth.user.OAuth2UserInfo;
 import com.e103.ohmyguide.domain.auth.user.OAuth2UserInfoFactory;
 import com.e103.ohmyguide.domain.user.entity.User;
-import com.e103.ohmyguide.domain.user.repository.UserRepository;
-import com.e103.ohmyguide.global.exception.OAuth2AuthenticationProcessingException;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
@@ -16,17 +14,13 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-    private final UserRepository userRepository;
+    private final OAuth2UserProcessingService oauth2UserProcessingService;
 
-    //백엔드 리다이렉션 페이지에서 토큰을 받은 후 리소스에 다시 요청해서 유저 정보를 받아옴
     @Override
     public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
@@ -35,53 +29,16 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         } catch (AuthenticationException ex) {
             throw ex;
         } catch (Exception ex) {
-            // Throwing an instance of AuthenticationException will trigger the OAuth2AuthenticationFailureHandler
             throw new InternalAuthenticationServiceException(ex.getMessage(), ex.getCause());
         }
     }
 
     private OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
         OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest, oAuth2User.getAttributes());
+        AuthProvider provider = AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId());
 
-        if(StringUtils.isEmpty(oAuth2UserInfo.getEmail())) {
-            throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
-        }
-
-        Optional<User> userOptional = userRepository.findByEmail(oAuth2UserInfo.getEmail());
-        User user;
-        if(userOptional.isPresent()) {
-            user = userOptional.get();
-            if(!user.getProvider().equals(AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId()))) {
-                throw new OAuth2AuthenticationProcessingException("Looks like you're signed up with " +
-                        user.getProvider() + " account. Please use your " + user.getProvider() +
-                        " account to login.");
-            }
-            user = updateExistingUser(user, oAuth2UserInfo);
-        } else {
-            user = registerNewUser(oAuth2UserRequest, oAuth2UserInfo);
-        }
+        User user = oauth2UserProcessingService.processOAuth2User(provider, oAuth2UserInfo);
 
         return UserPrincipal.create(user, oAuth2User.getAttributes());
     }
-
-    private User registerNewUser(OAuth2UserRequest oAuth2UserRequest, OAuth2UserInfo oAuth2UserInfo) {
-        User user = User.oauth2Builder()
-                .provider(AuthProvider.valueOf(oAuth2UserRequest.getClientRegistration().getRegistrationId()))
-                .providerId(oAuth2UserInfo.getId())
-                .name(oAuth2UserInfo.getName())
-                .email(oAuth2UserInfo.getEmail())
-                .imageUrl(oAuth2UserInfo.getImageUrl())
-                .build();
-
-        return userRepository.save(user);
-    }
-
-    private User updateExistingUser(User existingUser, OAuth2UserInfo oAuth2UserInfo) {
-        existingUser.updateOAuth2UserInfo(
-                oAuth2UserInfo.getName(),
-                oAuth2UserInfo.getImageUrl()
-        );
-        return userRepository.save(existingUser);
-    }
-
 }
