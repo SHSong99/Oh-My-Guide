a4bb4547aebdc941486862d631559a8108d43fc1
joonwan1024 <joownan1024@naver.com>
2026-03-22 22:43:52 +0900

    feat: bookmark 기능 개발

--- backend/src/main/java/com/e103/ohmyguide/domain/userphrase/repository/UserPhraseRepository.java
+++ backend/src/main/java/com/e103/ohmyguide/domain/userphrase/repository/UserPhraseRepository.java
@@ -1,7 +1,13 @@
 package com.e103.ohmyguide.domain.userphrase.repository;
 
+import com.e103.ohmyguide.domain.phrase.entity.Phrase;
+import com.e103.ohmyguide.domain.user.entity.User;
 import com.e103.ohmyguide.domain.userphrase.entity.UserPhrase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserPhraseRepository extends JpaRepository<UserPhrase, Long> {
+
+    boolean existsByUserAndPhrase(User user, Phrase phrase);
+
+    void deleteByUserAndPhrase(User user, Phrase phrase);
 }
