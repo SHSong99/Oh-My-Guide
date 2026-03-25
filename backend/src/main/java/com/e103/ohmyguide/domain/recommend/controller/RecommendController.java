d9df61d30b8e9c2c91509facd06cf7632cd87b73
이정현 <dlwjdgus2002@naver.com>
2026-03-25 09:24:27 +0900

    Revert "Merge branch 'master' into 'frontend_develop'"
    This reverts merge request !131

--- backend/src/main/java/com/e103/ohmyguide/domain/recommend/controller/RecommendController.java
+++ backend/src/main/java/com/e103/ohmyguide/domain/recommend/controller/RecommendController.java
@@ -7,11 +7,9 @@ import com.e103.ohmyguide.domain.recommend.dto.RefreshResponse;
 import com.e103.ohmyguide.domain.recommend.dto.VisitRequest;
 import com.e103.ohmyguide.domain.recommend.service.RecommendService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
 @RestController
 @RequestMapping("/userRecommend")
 @RequiredArgsConstructor
@@ -26,13 +24,9 @@ public class RecommendController {
             @RequestParam Double currentLat,
             @RequestParam Double currentLng
     ) {
-
-        log.info("RecommendController.getRecommendation: 사용자 추천 요청 lat = {}, lot = {}", currentLat, currentLng);
         RefreshResponse response = recommendService.getRecommendation(
                 userPrincipal.getId(), category, currentLat, currentLng
         );
-
-        log.info("RecommendController.getRecommendation: size = {} ", response.getRecommendations().size());
         return ResponseEntity.ok(response);
     }
 
@@ -44,7 +38,6 @@ public class RecommendController {
         RefreshResponse response = recommendService.refreshRecommendation(
                 userPrincipal.getId(), request
         );
-        log.info("RecommendController.refreshRecommendation: size = {} ", response.getRecommendations().size());
         return ResponseEntity.ok(response);
     }
 
